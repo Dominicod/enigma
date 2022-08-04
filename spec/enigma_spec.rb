33cb2e47873ca7e302fb9318d5b5f186b6b2a54a
dominicod <dominicodonnell99@gmail.com>
2022-08-04 17:06:10 -0500

    feat: test wrote for #encrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,15 +1,25 @@
 require 'spec_helper'
 require_relative '../lib/enigma'
-let(:enigma) {enigma = Enigma.new}
 
 RSpec.describe Enigma do
+  let(:enigma) {enigma = Enigma.new}
+  
   describe '.enigma_intialize' do
     it 'instantiates' do
       expect(enigma).to be_instance_of described_class
     end
+  end
 
-    it 'returns instance variables' do
-      # TODO: returns instance variables
+  describe '#encrypt' do
+    it 'encrypts' do
+      encrypted_hash = 
+      {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+
+      expect(enigma.encrypt("hello world", "02715", "040895")).to eq encrypted_hash
     end
   end
 end
