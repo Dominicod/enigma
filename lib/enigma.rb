4cbd5f25e98355cb278832631666210e306c35f1
dominicod <dominicodonnell99@gmail.com>
2022-08-04 17:13:31 -0500

    feat: skeleton created for #encrypt/#decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,16 @@
 class Enigma
   def initialize
-    
+  end
+
+  def encrypt(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
+
+  def decrypt(ciper, key, date)
+    @ciper = ciper
+    @key = key
+    @date = date
   end
 end
