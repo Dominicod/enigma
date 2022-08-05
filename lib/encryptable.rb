56d7fe4d6003fa7c96c6a864507a65b0dc7f31ef
dominicod <dominicodonnell99@gmail.com>
2022-08-05 18:42:09 -0500

    #encrypt_message complete, and #shift_letter helper method complete

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -4,20 +4,31 @@ module Encryptable
 
   def self.encrypt_message(encrypt_message, key, date)
     final_shift = Shiftable.shift(date, key)
-    message = encrypt_message.split('')
-    count = 0
-    encrypted_message = message.map do |letter|
-      letter.ord >= 97 && letter.ord <= 122 ? shift_letter(letter, count) : encrypted_message << letter
-    end
-    encrypted_message.join
+    shift_letters = final_shift.keys
+    message = encrypt_message.downcase
+    character_set = ("a".."z").to_a << " "
+    encrypted_message = shift_letter(final_shift, message, shift_letters, character_set)
   end
 
   def self.decrypt_message(decrypt_message, key, date)
     final_shift = Shiftable.shift(date, key)
   end
 
-  def self::shift_letter(letter, count)
-    counter += 1
-    (letter.ord + final_shift[counter]).chr
+  def self::shift_letter(final_shift, message, shift_letters, character_set)
+    encryption = Array.new
+    counter = 0
+    message.each_char.with_index do |letter, index|
+      counter = 0 if counter == 4
+      if letter.ord >= 97 && letter.ord <= 122
+        set_index = character_set.find_index(letter)
+        encryption << character_set.rotate(final_shift[shift_letters[counter]] + set_index).first
+        counter += 1
+      else
+        encryption << letter
+        counter += 1
+      end
+    end
+      encryption.join
+      require 'pry'; binding.pry
   end
 end
