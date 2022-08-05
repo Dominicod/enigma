6483d09da55e9636df549740ef9cda7f18e18f20
dominicod <dominicodonnell99@gmail.com>
2022-08-05 14:46:23 -0500

    fix: test for shiftable altered to use helper method in shiftable. feat: helper method in shiftable made

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -1,9 +1,12 @@
+require_relative 'shiftable'
 module Encryptable
+  include Shiftable
   def self.encrypt_message(encrypt_message, key, date)
-    
+    final_shift = Shiftable.shift(date, key)
+    require 'pry'; binding.pry
   end
 
   def self.decrypt_message(decrypt_message, key, date)
-
+    require 'pry'; binding.pry
   end
 end
--- lib/shiftable.rb
+++ lib/shiftable.rb
@@ -1,14 +1,15 @@
 module Shiftable
-  def self.shift_key(key)
-    shift_key = {A: key[0..1], B: key[1..2], C: key[2..3], D: key[3..4]}
+  def self::shift_key(key)
+    {A: key[0..1], B: key[1..2], C: key[2..3], D: key[3..4]}
   end
 
-  def self.offset(date, shift_key)
+  def self.shift(date, key)
+    final_shift = shift_key(key)
     numeric_form = (date.to_i * date.to_i).to_s[-4..-1]
-    shift_key[:A] = (numeric_form[0].to_i + shift_key[:A].to_i)
-    shift_key[:B] = (numeric_form[1].to_i + shift_key[:B].to_i)
-    shift_key[:C] = (numeric_form[2].to_i + shift_key[:C].to_i)
-    shift_key[:D] = (numeric_form[3].to_i + shift_key[:D].to_i)
-    shift_key
+    final_shift[:A] = (numeric_form[0].to_i + final_shift[:A].to_i)
+    final_shift[:B] = (numeric_form[1].to_i + final_shift[:B].to_i)
+    final_shift[:C] = (numeric_form[2].to_i + final_shift[:C].to_i)
+    final_shift[:D] = (numeric_form[3].to_i + final_shift[:D].to_i)
+    final_shift
   end
 end
