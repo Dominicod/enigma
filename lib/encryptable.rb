8f114fd64f0a2cea85d303601f9451fc1cd3fbed
dominicod <dominicodonnell99@gmail.com>
2022-08-05 16:42:26 -0500

    feat: mocked up #shift_letter helper method

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -5,8 +5,9 @@ module Encryptable
   def self.encrypt_message(encrypt_message, key, date)
     final_shift = Shiftable.shift(date, key)
     message = encrypt_message.split('')
+    count = 0
     encrypted_message = message.map do |letter|
-      letter.ord >= 97 && letter.ord <= 122 ? shift_letter(letter) : encrypted_message << letter
+      letter.ord >= 97 && letter.ord <= 122 ? shift_letter(letter, count) : encrypted_message << letter
     end
     encrypted_message.join
   end
@@ -15,7 +16,8 @@ module Encryptable
     final_shift = Shiftable.shift(date, key)
   end
 
-  def self::shift_letter(letter)
-    ("h".ord + final_shift[:A]).chr
+  def self::shift_letter(letter, count)
+    counter += 1
+    (letter.ord + final_shift[counter]).chr
   end
 end
