63cae7f2361058b3a0caab74d3af27fc453fffe3
dominicod <dominicodonnell99@gmail.com>
2022-08-06 14:10:22 -0500

    feat: decryptor logic complete

--- decryptor.rb
+++ decryptor.rb
@@ -2,4 +2,14 @@ require_relative 'lib/enigma.rb'
 
 enigma = Enigma.new
 
-require 'pry'; binding.pry
+read_file = ARGV[0]
+write_file = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+
+message = (File.readlines(read_file).map(&:chomp)).join
+hash = enigma.decrypt(message, key, date)
+
+File.write(write_file, hash[:message])
+
+puts "Created '#{write_file}' with the key #{hash[:key]} and date #{hash[:date]}"
