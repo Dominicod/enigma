d7654d128364dfa591dc940b1aef0314f4e97412
dominicod <dominicodonnell99@gmail.com>
2022-08-07 15:44:58 -0500

    feat: created #crypt helper method to reduce DRY

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -1,39 +1,36 @@
 require_relative 'shiftable'
 
 module Encryptable
-  def self.encrypt_message(encrypt_message, key, date)
+  def self.encrypt_message(message, key, date)
     final_shift = Shiftable.shift(date, key)
-    encryption = Array.new
-    counter = 0
-    shift = final_shift.values
-    encrypt_message.downcase.each_char.with_index do |letter, index|
-      counter = 0 if counter == 4
-      shift(letter, shift, encryption, counter)
-      counter += 1
-    end
-      encryption.join
+    shift_values = final_shift.values
+    crypt(message, shift_values)
   end
 
-  def self.decrypt_message(decrypt_message, key, date)
+  def self.decrypt_message(message, key, date)
     final_shift = Shiftable.shift(date, key)
-    decryption = Array.new
-    counter = 0
-    shift = (final_shift.transform_values {|v| -v}).values
-    decrypt_message.downcase.each_char.with_index do |letter, index|
-      counter = 0 if counter == 4
-      shift(letter, shift, decryption, counter)
-      counter += 1
-    end
-      decryption.join
+    shift_values = (final_shift.transform_values {|v| -v}).values
+    crypt(message, shift_values)
   end
 
-  def self::shift(letter, shift, cryption, counter)
+  def self::shift(letter, shift_values, cryption, counter)
       if letter.ord >= 97 && letter.ord <= 122
         character_set = ("a".."z").to_a << " "
         set_index = character_set.find_index(letter)
-        cryption << character_set.rotate(shift[counter] + set_index).first
+        cryption << character_set.rotate(shift_values[counter] + set_index).first
       else
         cryption << letter
       end
+  end
+
+  def self::crypt(message, shift_values)
+    cryption = Array.new
+    counter = 0
+    message.downcase.each_char.with_index do |letter, index|
+      counter = 0 if counter == 4
+      shift(letter, shift_values, cryption, counter)
+      counter += 1
+    end
+    cryption.join
   end
 end
